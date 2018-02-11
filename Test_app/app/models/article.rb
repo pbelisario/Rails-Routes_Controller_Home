42c93cc5c3bba067479b52a9ab16ef51a3bf912b
pedrobelisario1997 <pedrobelisario1997@gmail.com>
2018-02-11 09:05:45 -0500

    Create articles table, model and add validation

--- Test_app/app/models/article.rb
+++ Test_app/app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ActiveRecord::Base
   
+  validades :title, presence: true, length: { minimum: 3, maximum: 50 }
+  # Garante que exista um titulo antes que seja salvo no banco de dados
+  validades :description, presence: false, length: { minimum 10, maximum: 300 }
+  
+  
 end
